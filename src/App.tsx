32e40b71a1f7559904bc4787fd6c48ccb152eac7
Gabriel <gbryan6@hotmail.com>
2020-10-13 14:52:50 -0300

    feat: build routes, create components

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,15 @@
 import React from 'react';
-import {FiArrowRight} from 'react-icons/fi';
+
 
 import './styles/global.css';
-import './styles/pages/landing.css';
+import Routes from './routes';
 
 
 
-import logoImg from './images/Logo.svg';
+
 function App() {
   return (
-    <div id="page-landing">
-      <div className="content-wrapper">
-        <img src={logoImg} alt="Logotipo happy" />
-
-        <main>
-          <h1>
-            Leve felicidade para o mundo.
-          </h1>
-          <p>
-            Visite orfanatos e mude o dia de muitas crianças.
-          </p>
-        </main>
-
-        <div className="location">
-          <strong>Ribeirão das Neves</strong>
-          <span>Minas Gerais</span>
-        </div>
-
-        <a href="/" className="enter-app">
-          <FiArrowRight size={26} color='rgba(0, 0, 0, 0.6)' />
-        </a>
-      </div>
-    </div>
+    <Routes />
   );
 }
 
